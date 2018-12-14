db92bcd0b23b4bc9996aa279d73da8ebc633676f
Junichiro Sakama <6518006s@st.toho-u.ac.jp>
2018-12-14 15:59:38 +0900

    first commit

---
+++ Assets/Scripts/StoryManagement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StoryManagement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	string[] messages = { "えるしっているか",
+	                      "しにがみはりんごを",
+	                      "りんご" };
+	int messageIndex = 0;
+
+	public GameObject imageBackground;
+	public Sprite[] background = new Sprite[3];
+	public GameObject buttonMessage;
+	public GameObject buttonMessageText;
+
+	public void DisplayMessage() {
+		if (messages.Length <= messageIndex) {
+			buttonMessage.SetActive(false);
+		} else {
+			buttonMessageText.GetComponent<Text>().text = messages[messageIndex];
+			imageBackground.GetComponent<Image>().sprite = background[messageIndex];
+			messageIndex++;
+		}
+	}
+}
